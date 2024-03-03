943977fddfc26da44739dd33cac38dd089f3bb85
vasileva65 <vasilieva.alena65@gmail.com>
2024-03-03 14:56:11 +0500

    rusification

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -9,18 +9,22 @@ from django.core.validators import MinLengthValidator
 from django.utils import timezone
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=150, validators=[
+    first_name = models.CharField(max_length=150, verbose_name='Имя', validators=[
         MinLengthValidator(limit_value=1, message=("Имя не может быть пустым."))
     ])
-    last_name = models.CharField(max_length=100, validators=[
+    last_name = models.CharField(max_length=100, verbose_name='Фамилия', validators=[
     MinLengthValidator(limit_value=1, message=("Фамилия не может быть пустой."))
 ])
-    middle_name = models.CharField(max_length=150, blank=True)
+    middle_name = models.CharField(max_length=150, blank=True, verbose_name='Отчество')
 
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
     def __str__(self):
         return self.username
@@ -30,16 +34,23 @@ class User(AbstractUser):
         self.lastname = self.last_name.capitalize()
         self.middle_name = self.middle_name.capitalize()
         super().save(*args, **kwargs)
+
+    
     
 class UserProfile(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to ='user_photos/', default='user_photos/default.jpg', height_field=None, width_field=None)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, null=True, verbose_name='Время обновления')
+
+    class Meta:
+        verbose_name = 'Профиль пользователя'
+        verbose_name_plural = 'Профили пользователей'
 
     def __str__(self):
         return str(self.user)
-
+    
+    
 #create profile when user signs up
 def create_profile(sender, instance, created, **kwargs):
     if created:
@@ -49,24 +60,35 @@ def create_profile(sender, instance, created, **kwargs):
 post_save.connect(create_profile, sender=User)
 
 class Chat(models.Model):
-    chat_id = models.AutoField(primary_key=True)
-    chat_name = models.CharField(max_length=50, unique=True, null=True, blank=True)
-    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    chat_id = models.AutoField(primary_key=True, verbose_name='ID чата')
+    chat_name = models.CharField(max_length=50, unique=True, null=True, blank=True, verbose_name='Название')
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='ID пользователя')
     avatar = models.ImageField(upload_to ='chat_photos/', default='chat_photos/default.jpg', null=True,
-        blank=True, height_field=None, width_field=None)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
-    group_chat = models.BooleanField(default=False)
+        blank=True, height_field=None, width_field=None, verbose_name='Аватар')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, null=True, verbose_name='Время обновления')
+    group_chat = models.BooleanField(default=False, verbose_name='Является ли групповым чатом')
     # TODO: add last read concept
     
+    class Meta:
+        verbose_name = 'Чат'
+        verbose_name_plural = 'Чаты'
+
+
     def __str__(self):
         return self.chat_name
+    
 
 class ChatAdmins(models.Model):
-    chat_id = models.ForeignKey(Chat, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    joined_at = models.DateTimeField(auto_now_add=True)
-    left_at = models.DateTimeField(null=True, blank=True)
+    chat_id = models.ForeignKey(Chat, on_delete=models.CASCADE, verbose_name='ID чата')
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='ID пользователя')
+    joined_at = models.DateTimeField(auto_now_add=True, verbose_name='Время становления администратором')
+    left_at = models.DateTimeField(null=True, blank=True, verbose_name='Время лишения прав администратора')
+
+    class Meta:
+        verbose_name = 'Администратор чата'
+        verbose_name_plural = 'Администраторы чата'
+
 
     def __str__(self):
         return self.chat_id.chat_name + ' ' + self.user_id.username
@@ -75,13 +97,17 @@ class ChatAdmins(models.Model):
         if not self.pk:
             self.left_at = None
         super().save(*args, **kwargs)
+    
 
 class ChatMembers(models.Model):
-    chat_id = models.ForeignKey(Chat, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    joined_at = models.DateTimeField(auto_now_add=True)
-    left_at = models.DateTimeField(null=True, blank=True)
+    chat_id = models.ForeignKey(Chat, on_delete=models.CASCADE, verbose_name='ID чата')
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='ID пользователя')
+    joined_at = models.DateTimeField(auto_now_add=True, verbose_name='Время присоединения')
+    left_at = models.DateTimeField(null=True, blank=True, verbose_name='Время выхода из чата')
 
+    class Meta:
+        verbose_name = 'Участник чата'
+        verbose_name_plural = 'Участники чата'
 
     def __str__(self):
         return self.chat_id.chat_name + ' ' + self.user_id.username
@@ -90,17 +116,21 @@ class ChatMembers(models.Model):
         if not self.pk:
             self.left_at = None
         super().save(*args, **kwargs)
-    
+
 
 class Message(models.Model):
-    message_id = models.AutoField(primary_key=True)
-    sender_id = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='message', on_delete=models.CASCADE)
-    chat_id = models.ForeignKey(Chat, on_delete=models.CASCADE)
-    body = models.TextField(validators=[validate_is_profane])
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
+    message_id = models.AutoField(primary_key=True, verbose_name='ID сообщения')
+    sender_id = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='message', on_delete=models.CASCADE, verbose_name='ID отправителя')
+    chat_id = models.ForeignKey(Chat, on_delete=models.CASCADE, verbose_name='ID чата')
+    body = models.TextField(validators=[validate_is_profane], verbose_name='Содержание сообщения')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, null=True, verbose_name='Время обновления')
     #is_read = models.BooleanField(default=False)
     # TODO: add position concept
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
 
     def __str__(self):
         return self.chat_id.chat_name
@@ -110,14 +140,19 @@ class Message(models.Model):
 
 
 class ActionLog(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    action_type = models.CharField(max_length=255)
-    target_object_id = models.PositiveIntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='ID пользователя')
+    action_type = models.CharField(max_length=255, verbose_name='Тип действия')
+    target_object_id = models.PositiveIntegerField(verbose_name='ID объекта действия')
     timestamp = models.DateTimeField(default=timezone.now)
+    # action_data = models.JSONField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.user} - {self.action_type} on {self.target_object_id}"
 
+    class Meta:
+        verbose_name = 'Журнал действий'
+        verbose_name_plural = 'Журналы действий'
+        
 
 class Notification(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- backend/chat/signals.py
+++ backend/chat/signals.py
@@ -5,53 +5,21 @@ from .models import Chat, ChatAdmins, ChatMembers, User, ActionLog
 
 @receiver(post_delete, sender=Chat)
 def log_chat_deletion(sender, instance, **kwargs):
-    ActionLog.objects.create(user=instance.user_id, action_type='Chat Deleted', target_object_id=instance.chat_id)
+    ActionLog.objects.create(user=instance.user_id, action_type='Удален чат', target_object_id=instance.chat_id)
 
 @receiver(post_delete, sender=ChatAdmins)
 def log_chat_admin_removal(sender, instance, **kwargs):
-    ActionLog.objects.create(user=instance.user_id, action_type='Admin Removed from Chat', target_object_id=instance.chat_id)
+    ActionLog.objects.create(user=instance.user_id, action_type='Удален администратор группы', target_object_id=instance.chat_id)
 
 @receiver(post_delete, sender=ChatMembers)
 def log_chat_member_removal(sender, instance, **kwargs):
-    ActionLog.objects.create(user=instance.user_id, action_type='Member Removed from Chat', target_object_id=instance.chat_id)
+    ActionLog.objects.create(user=instance.user_id, action_type='Удален участник группы', target_object_id=instance.chat_id)
 
 @receiver(post_delete, sender=User)
 def log_user_deletion(sender, instance, **kwargs):
-    ActionLog.objects.create(user=instance, action_type='User Deleted', target_object_id=instance.user_id)
+    ActionLog.objects.create(user=instance, action_type='Удален пользователь', target_object_id=instance.user_id)
 
 @receiver(post_save, sender=Chat)
 def log_chat_creation(sender, instance, created, **kwargs):
     if created:
-        ActionLog.objects.create(user=instance.user_id, action_type='Chat Created', target_object_id=instance.chat_id)
-
-# from django.db.models.signals import post_save, post_delete
-# from django.dispatch import receiver
-# from django.apps import apps
-# from django.apps import AppConfig
-# from chat.models import *
-# from django.core import serializers
-
-# @receiver(post_save, sender=Chat)
-# @receiver(post_save, sender=ChatAdmins)
-# @receiver(post_save, sender=ChatMembers)
-# @receiver(post_save, sender=User)
-# @receiver(post_delete, sender=Chat)
-# @receiver(post_delete, sender=ChatAdmins)
-# @receiver(post_delete, sender=ChatMembers)
-# @receiver(post_delete, sender=User)
-# def log_database_changes(sender, instance, **kwargs):
-#     # Serialize the instance to JSON
-#     serialized_instance = serializers.serialize('json', [instance])
-    
-#     # Get the app label and model name
-#     app_label = sender._meta.app_label
-#     model_name = sender.__name__
-
-#     # Log the action
-#     action_type = 'Create' if kwargs.get('created') else 'Update' if kwargs.get('updated') else 'Delete'
-#     ActionLog.objects.create(
-#         user=instance.user_id if hasattr(instance, 'user_id') else instance,  # Assuming the user field is present
-#         action_type=f"{action_type} {model_name}",
-#         target_object_id=instance.user_id,
-#         action_data=serialized_instance
-#     )
+        ActionLog.objects.create(user=instance.user_id, action_type='Создан чат', target_object_id=instance.chat_id)
