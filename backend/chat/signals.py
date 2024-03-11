e031088ea5350d30f232aa787cefda497d68747b
vasileva65 <vasilieva.alena65@gmail.com>
2024-03-11 21:19:57 +0500

    signals remove member/admin

--- backend/chat/signals.py
+++ backend/chat/signals.py
@@ -41,3 +41,27 @@ def log_chat_admin_addition(sender, instance, created, **kwargs):
             action_type='Добавлен администратор в чат',
             target_object_id=instance.chat_id.chat_id  # Используйте instance.id вместо instance.chat_id
         )
+
+@receiver(post_save, sender=ChatMembers)
+def log_chat_member_update(sender, instance, **kwargs):
+    print("called signal for member")
+    if instance.left_at:
+        print("if worked")
+        # Если left_at не установлено, значит, это обновление, а не создание
+        ActionLog.objects.create(
+            user=instance.user_id,
+            action_type='Удален участник группы',
+            target_object_id=instance.chat_id.chat_id
+        )
+
+@receiver(post_save, sender=ChatAdmins)
+def log_chat_admin_update(sender, instance, **kwargs):
+    print("called signal for admin")
+    if instance.left_at:
+        print("if worked")
+        # Если left_at не установлено, значит, это обновление, а не создание
+        ActionLog.objects.create(
+            user=instance.user_id,
+            action_type='Отозваны права администратора в группе',
+            target_object_id=instance.chat_id.chat_id
+        )
